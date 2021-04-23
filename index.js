576ac08b107eff8a1600d38b1281324e950a1674
Sravanth C <54331348+chebro@users.noreply.github.com>
2021-04-24 01:01:23 +0530

    fix: assign req.ipReal if no proxy

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
+	req.ipReal = req.ip;
 	if(req.headers['x-real-ip'])
 		req.ipReal = req.headers['x-real-ip'];
 	next();
