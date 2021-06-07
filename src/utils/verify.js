0f7b8efd61e9cc6a02d69ce25f9269f8af04f980
Sravanth C <54331348+chebro@users.noreply.github.com>
2021-06-08 01:25:36 +0530

    enhancement: set auth token in .env - discard src/token in favor of using dotenv - remove `token` from .gitignore (no longer necessary)

--- src/utils/verify.js
+++ src/utils/verify.js
@@ -1,21 +1,15 @@
-const path = require('path');
-const fs = require('fs');
-
 module.exports = (req, res, next) => {
-	fs.readFile(path.join(__dirname, '../token'), 'utf-8', (err, data) => {
-		if (err) throw err;
-		if (data === req.headers.auth) {
-			next();
-		} else {
-			console.log(
-				`${new Date().toLocaleString('en-GB')} - Incorrect token from ${req.ipReal} - ${
-					req.headers.auth
-				}`
-			);
-			res.status(401).json({
-				status: 'fail',
-				message: 'Invalid Credential',
-			});
-		}
-	});
+	if (process.env.AUTH === req.headers.auth) {
+		next();
+	} else {
+		console.log(
+			`${new Date().toLocaleString('en-GB')} - Incorrect token from ${req.ipReal} - ${
+				req.headers.auth
+			}`
+		);
+		res.status(401).json({
+			status: 'fail',
+			message: 'Invalid Credential',
+		});
+	}
 };
