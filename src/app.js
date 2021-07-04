927631ecf61d9cfb2c10dc6a1e9a9acb3169025f
Chebro <54331348+chebro@users.noreply.github.com>
2021-07-04 23:29:39 +0530

    add: trust proxy rule
    - removed unnecessary middleware in favor of using express.js' built in trust proxy feature.

--- src/app.js
+++ src/app.js
@@ -9,22 +9,17 @@ const statsRouter = require('./routes/statsRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', true);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
-app.use((req, _res, next) => {
-	req.ipReal = req.ip;
-	if (req.headers['x-real-ip']) req.ipReal = req.headers['x-real-ip'];
-	next();
-});
 
 app.use('/', mainRouter);
 app.use('/stats', statsRouter);
 
 app.use((req, res) => {
 	console.log(
-		`${new Date().toLocaleString('en-GB')} - Invalid request from ${req.ipReal} - ${
+		`${new Date().toLocaleString('en-GB')} - Invalid request from ${req.ip} - ${
 			req.originalUrl
 		}`
 	);
